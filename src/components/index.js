37510e5b61176995e9f4dbe01f3f0924caf14bca
Manikandan1818 <simbumanii18@gmail.com>
2023-12-13 19:08:52 +0530

    Puma app created and Nac component created!

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+import Nav from "./Nav";
+
+export {
+  Hero,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+  CustomerReviews,
+  Nav,
+};

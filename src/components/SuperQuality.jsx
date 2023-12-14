ad3b1f95fab4edbe69b316a6bea83cc258fa3ec3
Manikandan1818 <simbumanii18@gmail.com>
2023-12-14 22:18:40 +0530

    Super quality product working one

--- src/components/SuperQuality.jsx
+++ src/components/SuperQuality.jsx
@@ -1,5 +1,25 @@
+import { arrowRight } from "../assets/icons";
+import Button from "./Button";
+
 const SuperQuality = () => {
-  return <div>Super Quality</div>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We Provide you <span className="text-coral-red">Super</span>
+          <span className="text-coral-red"> Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Discover Stylish Puma arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="View Details" iconURL={arrowRight} />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
